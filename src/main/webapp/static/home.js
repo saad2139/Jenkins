7b6f7dbc10f39ecffb78ddbbd6d27f45fdd7c914
saad2139 <saad2139@gmail.com>
2017-12-03 17:59:16 -0500

    Update home.js

--- src/main/webapp/static/home.js
+++ src/main/webapp/static/home.js
@@ -3,12 +3,12 @@ function nextPage(button) {
     let xhttp = new XMLHttpRequest();
     switch (button.id) {
         case 'history':
-            xhttp.open('GET', 'http://localhost:8080/ReimbursementApp/home');
+            xhttp.open('GET', './home');
             xhttp.send();
             window.location = './history';
             break;
         case 'manageReimbursements':
-            xhttp.open('GET', 'http://localhost:8080/ReimbursementApp/home');
+            xhttp.open('GET', './home');
             xhttp.send();
             window.location = './manageReimbursements';
             break;
